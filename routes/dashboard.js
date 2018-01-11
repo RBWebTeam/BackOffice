372c53ef16b52ba5a5999b1b40baf264332dbfa7
durga pratap <scriptdp@gmail.com>
2018-01-11 13:06:54 +0530

    create header page

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,10 +1,24 @@
  var express = require('express');
  var router = express.Router();
  dbconnection=require('../bin/dbconnection.js');
+ var session = require('express-session');
 
+router.use(session({secret: "chaos is a ladder."}));
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    
-       res.render('dashboard', { title: 'Express' });
+router.get('/',isAuthenticated, function(req, res, next) {
+     
+    // console.log(req.session.name);
+       res.render('dashboard', { title: 'Express',Login:req.session.name });
 });
+
+
+function isAuthenticated(req, res, next) {
+
+	 console.log(req.session.name)
+  if (req.session.name)
+      return next();
+//  res.redirect('/');
+    res.send("Not authorized");
+}
+
 module.exports = router;
--- routes/login.js
+++ routes/login.js
@@ -1,30 +1,56 @@
  var express = require('express');
  var router = express.Router();
  dbconnection=require('../bin/dbconnection.js');
+ 
+var session = require('express-session');
+//var bodyParser = require('body-parser');
+//var app = express();
 
+ 
+router.use(session({ resave: true ,secret: '123456' , saveUninitialized: true}));
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('login', { title: 'Express' });
 });
 
 router.post('/login-authentication', function(req, res, next) {
+  sess = req.session;
     status=null;
     data =null;
-   dbconnection.query("SELECT  email,password  FROM registrations where email='"+req.body.email+"' and  password='"+req.body.pwd+"' ", function (err, result, fields) {     
+   dbconnection.query("SELECT  firstName,email  FROM registrations where email='"+req.body.email+"' and  password='"+req.body.pwd+"' ", function (err, result, fields) {     
               if (err) throw err; 
+                
+             //  sess={"email":result[0].email,"name":result[0].firstName};
+                     
                if(result.length > 0) {
                    status=0;
+                   sess.email=result[0].email;
+                   sess.name=result[0].firstName;
                    data="success";
                  }else{
                    data="Come on now! Stop kidding, Enter correct email and password.";
                    status=1;
                 } 
           
-          result={'status':status,'name':data};
-          res.send(result);
+         result={'status':status,'name':data};
+         res.send(result);
                 
   });
 
+});
+
+router.get('/log-out', function(req, res, next) {
+    
+    req.session.destroy(function(err){
+      if(err){
+        console.log(err);
+      }
+      else
+      {
+        res.redirect('/');
+      }
+    });
 
 });
+
 module.exports = router;
